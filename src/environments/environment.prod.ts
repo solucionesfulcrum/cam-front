498f5a846f99b3e781e5a94d4b352810ce8d8621
Diego-Pz <diegopaz25814@gmail.com>
2024-02-20 09:45:47 -0500

    Fix de servicios en QA

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production: true,
-  API: 'https://apps.essalud.gob.pe/sigps-service',
+  API: 'https://apps.essalud.gob.pe/cam-service',
   API_SEGURO: 'https://apps.essalud.gob.pe/sagw/sigps/viva-apidatosmaestros',
   API_ESSI: 'https://apps.essalud.gob.pe/sagw/servicios-essi/apimaestra',
   SSO_API: 'https://apps.essalud.gob.pe/sso-main/api',
