c8933ed74c7fbcd2257b541135d75d45a30353db
Diego-Pz <diegopaz25814@gmail.com>
2024-02-23 22:07:27 -0500

    Cambio menor en la adición de submenu en el sidenav

--- src/app/layout/sidenav/sidenav.component.ts
+++ src/app/layout/sidenav/sidenav.component.ts
@@ -1,16 +1,18 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { RouterModule } from '@angular/router';
+import { Router, RouterModule } from '@angular/router';
 import { SharedModule } from '@shared/shared.module';
 import { DatosGeneralesService } from 'src/app/data/services/datos-generales.service';
 import { AppRoute } from 'src/app/data/constants/app-route.constant';
 import { MaterialModule } from 'src/app/material/material.module';
 import { MatListModule } from '@angular/material/list';
+import { CdkMenuModule } from '@angular/cdk/menu';
+import { AuthService } from '@services/auth.service';
 
 @Component({
   selector: 'app-sidenav',
   standalone: true,
-  imports: [SharedModule, MatListModule],
+  imports: [SharedModule, MatListModule, CdkMenuModule],
   templateUrl: './sidenav.component.html',
   styleUrls: ['./sidenav.component.scss'],
 })
@@ -18,17 +20,21 @@ export class SidenavComponent {
   rutas = AppRoute;
 
   unidOpeUserSession!: string;
+  userInfo: any = Object();
   userCategoria: string = '';
   showUnidOpe: boolean = false;
 
-  constructor(private datosService : DatosGeneralesService){}
+  constructor(private datosService : DatosGeneralesService,
+              private authService:AuthService,
+              private router:Router){}
 
   ngOnInit(){
     console.log(JSON.parse(localStorage.getItem('UnidElegida')!));
-    // console.log((JSON.parse(localStorage.getItem('camUser')!)))
+    console.log((JSON.parse(localStorage.getItem('camUser')!)))
     if(localStorage.getItem('UnidElegida') != 'null'){
       // let idUnid: string;
       // this.userCategoria = (JSON.parse(localStorage.getItem('camUser')!)).categoria;
+      this.userInfo = (JSON.parse(localStorage.getItem('camUser')!));
       this.unidOpeUserSession = (JSON.parse(localStorage.getItem('UnidElegida')!)).unidOperativa;
       this.showUnidOpe = true;
       // idUnid = (JSON.parse(localStorage.getItem('camUser')!)).idUnidOperativa;
@@ -41,5 +47,10 @@ export class SidenavComponent {
       this.unidOpeUserSession = 'SEDE CENTRAL';
       this.showUnidOpe = true;
     }
+  }
+
+  logout(){
+    this.authService.logout()
+    this.router.navigate(['/login'])
   }
 }
