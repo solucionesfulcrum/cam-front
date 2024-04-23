e80fc8518e2e6a3184c2132a0f87a31d45dcb7ac
Kevin Arias <kevinariassistemas@gmail.com>
2024-04-22 23:24:24 -0500

    [feature]-carga

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,10 +1,10 @@
 export const environment = {
   production: false,
-  //API: 'https://appsqa.essalud.gob.pe/cam-service',
+  API: 'https://appsqa.essalud.gob.pe/cam-service',
   // API_SEGURO: 'https://appsqa.essalud.gob.pe/sagw/sigps/viva-apidatosmaestros',
   // API_ESSI: 'https://appsqa.essalud.gob.pe/sagw/servicios-essi/apimaestra',
   // SSO_API: 'https://appsqa.essalud.gob.pe/sagw/sso-main/api',
-  API: 'http://localhost:8085',
+  //API: 'http://localhost:8085',
   environmentName: 'Dev-Local',
 
 
