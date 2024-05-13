9a9917226a7a50367cbe136cab933c57d89e62f1
Diego-Pz <diegopaz25814@gmail.com>
2024-05-13 11:03:58 -0500

    Cambios Modulo de Gestión

--- src/app/pages/control/control/control.component.ts
+++ src/app/pages/control/control/control.component.ts
@@ -7,6 +7,8 @@ import { FormatoTab } from '@shared/components/menu-opciones/formato-tab.model';
   styleUrls: ['./control.component.scss']
 })
 export class ControlComponent {
+  
+  userInfo = (JSON.parse(localStorage.getItem('UnidElegida')!));
   links: FormatoTab[] = [
     {url: `/app/control`, title: 'Programados'},
     /*{url: `/app/`, title: 'En Calendario'},
@@ -14,4 +16,9 @@ export class ControlComponent {
     {url: `/app/`, title: 'Talleres'},
     {url: `/app/`, title: 'Asistencias'},*/
   ]
+  ngOnInit(){
+    if (this.userInfo.rol === 'TALLERISTA') {
+      this.links[0].title = 'Mis Talleres';
+    }
+  }
 }
