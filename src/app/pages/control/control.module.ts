2a6c797d8c8363fe94188ecf2970450b79e6d98d
lldisool1997 <56740315+lldisool1997@users.noreply.github.com>
2024-05-17 06:49:36 -0500

    CORRENCION PUSH

--- src/app/pages/control/control.module.ts
+++ src/app/pages/control/control.module.ts
@@ -6,15 +6,12 @@ import { ControlComponent } from './control/control.component';
 import { ProgramadosComponent } from './components/programados/programados.component';
 import { TabMisTalleresComponent } from './components/tab-mis-talleres/tab-mis-talleres.component';
 import { TabAsistenciaComponent } from './components/tab-asistencia/tab-asistencia.component';
-<<<<<<< HEAD
 import { InscripcionModalComponent } from './modals/inscripcion-modal/inscripcion-modal.component';
-import { MatAutocompleteModule } from '@angular/material/autocomplete';
 import { MaterialModule } from 'src/app/material/material.module';
 import { TabAsistenciaProfCamComponent } from './components/tab-asistencia-prof-cam/tab-asistencia-prof-cam.component';
-=======
 import { DialogConfirmDataAsistenciaComponent } from './components/tab-asistencia/dialog/dialog-confirm-data-asistencia/dialog-confirm-data-asistencia.component';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
->>>>>>> 335cf3e63b21806f83aea73b004a248643c60187
+
 
 
 @NgModule({
@@ -23,23 +20,16 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     ProgramadosComponent,
     TabMisTalleresComponent,
     TabAsistenciaComponent,
-<<<<<<< HEAD
     InscripcionModalComponent,
-    TabAsistenciaProfCamComponent
-=======
+    TabAsistenciaProfCamComponent,
     DialogConfirmDataAsistenciaComponent
->>>>>>> 335cf3e63b21806f83aea73b004a248643c60187
   ],
   imports: [
     CommonModule,
     ControlRoutingModule,
     SharedModule,
-<<<<<<< HEAD
     MaterialModule,
-    MatAutocompleteModule,
-=======
     MatAutocompleteModule
->>>>>>> 335cf3e63b21806f83aea73b004a248643c60187
   ]
 })
 export class ControlModule { }
