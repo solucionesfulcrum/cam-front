7da77a9e04b5531228680a4348580cc82cdf7393
lldisool1997 <56740315+lldisool1997@users.noreply.github.com>
2024-06-27 02:43:28 -0500

    agregar el ícono de asistencia registrada en otro dia

--- src/app/pages/reportes-tallerista/detalle-asistencias-taller/detalle-asistencias-taller.component.ts
+++ src/app/pages/reportes-tallerista/detalle-asistencias-taller/detalle-asistencias-taller.component.ts
@@ -147,7 +147,9 @@ export class DetalleAsistenciasTallerComponent {
         this.reportService.getDataReporteAsistenciaTaller(this.idControlAsistenciaDet).subscribe((data)=>{
           this.loadingData = false;
           if (data.code == 0) {
-            this.dataSource = data.data;
+            this.dataSource = data.data.map(data => {
+              return {...data, agregadoFueraDeFecha: this.fechaServicio != data.fechaHoraAsistencia.split("T")[0]}
+            });
             this.total= data.data.length;
           }
           else {
