678aa52d2b2cc9992a5952b08ae262be57285108
Llento Soto <lldisool1997@gmail.com>
2024-09-02 05:21:22 -0500

    Revert "Merge branch 'revert-58b1fea5' into 'dev'"
    This reverts merge request !105

--- src/app/pages/auth/auth.module.ts
+++ src/app/pages/auth/auth.module.ts
@@ -25,6 +25,7 @@ import { DialogCodigoActivacionComponent } from './components/dialog-codigo-acti
 import { DialogModule } from '@angular/cdk/dialog';
 import { ToastrModule, ToastrService } from 'ngx-toastr';
 import { MatAutocompleteModule } from '@angular/material/autocomplete';
+import { MantenimientoComponent } from './components/mantenimiento/mantenimiento.component';
 
 
 @NgModule({
@@ -41,7 +42,8 @@ import { MatAutocompleteModule } from '@angular/material/autocomplete';
     LoginFormComponent,
     RecoveryFormComponent,
     RegisterFormComponent,
-    DialogCodigoActivacionComponent
+    DialogCodigoActivacionComponent,
+    MantenimientoComponent
   ],
   imports: [
     CommonModule,
