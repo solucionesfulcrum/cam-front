61edc0231598843fef1656dabeb4a01be20563b4
lldisool1997 <lldisool1997@gmail.com>
2024-10-23 14:53:13 -0500

    seguro retroactivo fix

--- src/app/pages/contactos/components/contactos-afiliados/sub-components/show-afiliado/show-afiliado.component.ts
+++ src/app/pages/contactos/components/contactos-afiliados/sub-components/show-afiliado/show-afiliado.component.ts
@@ -146,14 +146,14 @@ export class ShowAfiliadoComponent implements OnInit {
 
        this.aseguradoServices.getDatoSeguro(tipoDoc, String(this.dataFichaAfiliado.asegurado.numDoc)).subscribe(dataSeguro => {
         if(dataSeguro.data.length > 0){
-          if(dataSeguro.data[0].TIPO_ASEGURADO != this.dataFichaAfiliado.asegurado.descTipoSeguro){
+          if(dataSeguro.data[0].DGACTAS + " " + dataSeguro.data[0].TIPO_ASEGURADO != this.dataFichaAfiliado.asegurado.descTipoSeguro){
             this.aseguradoServices.corregirDatoSeguro({
               numDoc: String(this.dataFichaAfiliado.asegurado.numDoc),
               tipoDoc: tipoDoc,
               idAsegurado: this.dataFichaAfiliado.asegurado.idAsegurado
             }).subscribe(dataFix =>{
               if(dataFix.code == 0){
-                this.dataFichaAfiliado.asegurado.descTipoSeguro = dataSeguro.data[0].TIPO_ASEGURADO
+                this.dataFichaAfiliado.asegurado.descTipoSeguro = dataSeguro.data[0].DGACTAS + " " + dataSeguro.data[0].TIPO_ASEGURADO
               }
             })
           }
