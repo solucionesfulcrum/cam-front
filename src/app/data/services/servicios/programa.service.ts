c638db2a43d0720fd43349935228c151d58c999d
lldisool1997 <lldisool1997@gmail.com>
2024-12-13 04:59:12 -0500

    me muero

--- src/app/data/services/servicios/programa.service.ts
+++ src/app/data/services/servicios/programa.service.ts
@@ -4,6 +4,7 @@ import { environment } from '@environments/environment';
 import { listarProgramasRequest, listarSubProgramasRequest, ProgramaCrearRequestDto, ProgramaEditarRequestDto, ProgramaListadoItem, SubprogramaListadoItem } from '@models/cartera-de-servicios/cartera-de-servicios';
 import { DtGenericoPaginado, DtGenericoSinPaginar } from '@models/generico/dt-generico';
 import { ProgramacionRequestRegisterServicio } from '@models/programacion/programacion-contratos/programacion-contrato-lista.model';
+import { Observable } from 'rxjs';
 
 const URL_BASE = `${environment.API}/programa`;
 
@@ -14,23 +15,23 @@ export class ProgramaService {
 
   constructor(private _httpClient: HttpClient) { }
 
-  listarProgramas(model : listarProgramasRequest){
+  listarProgramas(model : listarProgramasRequest): Observable<DtGenericoPaginado<ProgramaListadoItem[]>>{
     const url = `${URL_BASE}/dt/listar`;
     return this._httpClient.post<DtGenericoPaginado<ProgramaListadoItem[]>>(url, model);
   }
 
-  listarActivos(){
+  listarActivos() : Observable<DtGenericoSinPaginar<any>>{
     const url = `${URL_BASE}/activos`;
     return this._httpClient.get<DtGenericoSinPaginar<any>>(url);
   }
 
 
-  registrarPrograma(model : ProgramaCrearRequestDto){
+  registrarPrograma(model : ProgramaCrearRequestDto): Observable<DtGenericoSinPaginar<any>>{
     const url = `${URL_BASE}/crear`;
     return this._httpClient.post<DtGenericoSinPaginar<any>>(url, model);
   }
 
-  editarPrograma(idPrograma: number, model : ProgramaEditarRequestDto){
+  editarPrograma(idPrograma: number, model : ProgramaEditarRequestDto) : Observable<DtGenericoSinPaginar<any>>{
     const url = `${URL_BASE}/editar/${idPrograma}`;
     return this._httpClient.put<DtGenericoSinPaginar<any>>(url, model);
   }
